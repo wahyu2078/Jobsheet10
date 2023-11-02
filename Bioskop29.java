31ce23944a07e2453984dabbef38a50a99c340ad
wahyu2078 <wahyutptp20@gmail.com>
2023-11-02 08:34:00 +0700

    Modifikasi3_Bioskop29

--- Bioskop29.java
+++ Bioskop29.java
@@ -17,14 +17,9 @@ public class Bioskop29 {
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
         System.out.println(penonton.length);
 
-        for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang baris ke-" + ": " + barisPenonton.length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
         }
 
-        System.out.println("Penonton pada baris ke-3: ");
-
-        for (String i : penonton[2]) {
-            System.out.println(i);
-        }
     }
 }
